caba77425bd45bd8213ea05cd17cf83aaf8993ad
Daniel Pace <danielpace6@gmail.com>
2015-05-08 17:07:16 -0700

    Use HTTPS web service URL.

--- App/scripts/definitions.js
+++ App/scripts/definitions.js
@@ -31,7 +31,7 @@ var VIBRATION_DURATION = 100;						// ms duration of device vibration when vibra
 var DB_NAME = "grab_tag";							// Name of the SQLite database for local data
 var MAX_LOCAL_TAGS = 5000;							// Max number of tags to be kept in local database
 var WEB_SERVICE_URL = 								// Web service URL for remote data
-		"http://www.kangaroostandard.com/GrabTag";
+		"https://www.kangaroostandard.com/GrabTag";
 var WEB_SERVICE_TIMEOUT = 10000;					// Web service calls time out after this many ms
 var TAG_LOAD_QUANTITY = 100;						// Number of tags requested in each web service call
 var TAG_RELOAD_QUANTITY = 50;						// Number of loaded tags left when more are needed
