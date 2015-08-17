bcc7121b9f2422b85acb0b16cf9343c8738ca3ef
Daniel Pace <danielpace6@gmail.com>
2015-08-17 13:25:33 -0700

    Add thousands separator to total tags in about screen.

--- App/scripts/menu.js
+++ App/scripts/menu.js
@@ -255,6 +255,9 @@ function showHelp() {
 // Shows the about window.
 //
 function showAbout() {
+	stats = {
+		all_time: 8500
+	};
 	showStandardDialog(
 		"Version " + APP_VERSION + "<br />" + 
 			"&copy; " + APP_COPYRIGHT_YEAR + " <a href=\"#\" onclick=\"window.open('" + APP_AUTHOR_LINK + "', '_system'); return false;\">" + APP_AUTHOR + "</a><br />" + 
@@ -264,7 +267,7 @@ function showAbout() {
 			(stats.today > 0? "Added today: <span class=\"stats stats-today\">" + stats.today + "</span><br />" : "") + 
 			(stats.this_week > 0? "In the last week: <span class=\"stats stats-this-week\">" + stats.this_week + "</span><br />" : "") + 
 			(stats.this_month > 0? "In the last 30 days: <span class=\"stats stats-this-month\">" + stats.this_month + "</span><br />" : "") + 
-			(stats.all_time > 0? "Total: <span class=\"stats stats-total\">" + stats.all_time + "</span><br />" : "")
+			(stats.all_time > 0? "Total: <span class=\"stats stats-total\">" + stats.all_time.toLocaleString() + "</span><br />" : "")
 			: ""), 
 		null, 
 		false, 
