6ba253ffb2003aa3b366cef67e21562e6192d0a4
Daniel Pace <danielpace6@gmail.com>
2015-09-08 11:57:16 -0700

    Remove phrase author from used tags dialog.

--- App/scripts/menu.js
+++ App/scripts/menu.js
@@ -204,7 +204,7 @@ function showUsedTags() {
 	for (var i = 0; i < usedTags.length; i++) {
 		var category = getCategoryById(usedTags[i].category_id);
 		message += "<a class=\"flag-tag\" href=\"#\" onclick=\"showTagFlaggingDialog(" + JSON.stringify(usedTags[i]).replace(/\"/g, "&quot;") + "); return false;\"><img src=\"images/flag.png\" alt=\"Flag\" title=\"Report this phrase\" /></a>";
-		message += htmlEncode(usedTags[i].text) + "<br /><span class=\"used-tag\">from <span class=\"tag-category\">" + category.name + "</span>" + (usedTags[i].authorName? "; submitted by <span class=\"tag-author\">" + usedTags[i].authorName + "</span>": "") + "</span>" + (i < usedTags.length - 1? "<hr />": "");
+		message += htmlEncode(usedTags[i].text) + "<br /><span class=\"used-tag\">from <span class=\"tag-category\">" + category.name + "</span></span>" + (i < usedTags.length - 1? "<hr />": "");
 	}
 
 	showStandardDialog(
