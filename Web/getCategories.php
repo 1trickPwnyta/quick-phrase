ea55ee7b3fc9804c08ab3cc8a9dcce1fa3ce6d68
Daniel Pace <danielpace6@gmail.com>
2015-12-24 08:21:57 -0800

    Fix bug that causes category IDs from the web service to be returned as strings.

--- Web/getCategories.php
+++ Web/getCategories.php
@@ -11,8 +11,10 @@ $query = "SELECT * FROM category ORDER BY name";
 $db = mysqlConnect();
 $statement = $db->query($query);
 $categories = array("");
-while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+	$row["id"] = intval($row["id"]);
 	array_push($categories, $row);
+}
 
 // Return the results
 echo json_encode($categories);
