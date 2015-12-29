a54bdbb52a3e568c6c95a8be96ad25f8812f743d
Daniel Pace <danielpace6@gmail.com>
2015-12-29 12:50:16 -0800

    Update version information.

--- App/scripts/definitions.js
+++ App/scripts/definitions.js
@@ -1,6 +1,6 @@
 // App information
 var APP_NAME = "Catch-Phrase Panic";
-var APP_VERSION = "2.0.0";
+var APP_VERSION = "2.0.1";
 var APP_AUTHOR = "Kangaroo Standard";
 var APP_AUTHOR_LINK = "https://www.kangaroostandard.com/";
 var APP_AUTHOR_EMAIL = "support@kangaroostandard.com";
