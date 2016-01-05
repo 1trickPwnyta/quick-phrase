39e569805a8627ec9466dfdbbd7f2a55cc68a8e1
Daniel Pace <danielpace6@gmail.com>
2016-01-05 13:27:53 -0800

    Add usage services into v2 web service.

--- Web/usage/clicks.php
+++ Web/usage/clicks.php
@@ -1,41 +1,5 @@
 <?php
-	include "../mysql.php";
-	require_once "../global.php";
-	
-	header("Access-Control-Allow-Origin: null");
-	
-	// Get parameters
-	$location = $_POST["location"];
-	
-	// Validate parameters
-	if ($location == "") {
-		http_response_code(400);
-		echo 0;
-		exit;
-	}
-	if (preg_match("/[^\\/\\w\\d\\.]/", $location)) {
-		http_response_code(400);
-		echo 0;
-		exit;
-	}
-	
-	$db = mysqlConnect();
-	
-	// Build a query
-	$query =
-		"INSERT INTO usage_clicks (location) VALUES (
-			{$db->quote($location)}
-		)";
 
-	// Insert the click into the database
-	$rowsAffected = $db->exec($query);
-
-	// Validate the result
-	if ($rowsAffected != 1) {
-		http_response_code(500);
-		echo 0;
-		exit;
-	}
-
-	// Return the results
-	echo 1;
+include "../mysql.php";
+include "../global.php";
+require "websvc/v2/usage/clicks.php";
